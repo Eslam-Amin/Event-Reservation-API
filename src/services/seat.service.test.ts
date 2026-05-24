66b4ebb4d960180d6e66b6f033c87897703c4d43
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-24 15:39:45 +0300

    test: create a starter for seat service

---
+++ src/services/seat.service.test.ts
@@ -0,0 +1,20 @@
+import { seatService } from "./seat.service";
+import { ApiError } from "../utils/ApiError";
+
+describe("🛡️ SeatService Unit Tests (v1.0.0)", () => {
+  let mockRepository: any;
+
+  beforeEach(() => {
+    mockRepository = {
+      getSeatForUpdate: jest.fn(),
+      updateSeatStatus: jest.fn(),
+      tx: jest.fn(async (callback) => callback({}))
+    };
+
+    seatService.seatRepository = mockRepository;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
