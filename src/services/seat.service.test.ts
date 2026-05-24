5b8c83dade7e442b539ae75240ec09655251e1e1
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-24 15:42:32 +0300

    test: create more test cases

--- src/services/seat.service.test.ts
+++ src/services/seat.service.test.ts
@@ -26,5 +26,34 @@ describe("SeatService Unit Tests (v1.0.0)", () => {
         ApiError.notFound("The requested seat does not exist for this event.")
       );
     });
+
+    it("should successfully update status if the seat is AVAILABLE", async () => {
+      const mockAvailableSeat = {
+        seat_number: "A-10",
+        id: 10,
+        status: "AVAILABLE",
+        event_id: 1
+      };
+      mockRepository.getSeatForUpdate.mockResolvedValue(mockAvailableSeat);
+
+      await seatService.reserveSeat(10, "buyer@test.com");
+
+      expect(mockRepository.updateSeatStatus).toHaveBeenCalled();
+    });
+
+    it("should throw a 400 error and not reserve seat if the seat is RESERVED", async () => {
+      const mockReservedSeat = {
+        id: 10,
+        seat_number: "A-10",
+        status: "RESERVED",
+        event_id: 1,
+        reserved_at: new Date()
+      };
+      mockRepository.getSeatForUpdate.mockResolvedValue(mockReservedSeat);
+
+      await expect(seatService.reserveSeat(10, "qa@test.com")).rejects.toThrow(
+        ApiError.badRequest("This seat is already reserved by another user.")
+      );
+    });
   });
 });
