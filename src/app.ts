825dcc45f9a4c4970ee485478e1ecce8483075db
Eslam Amin <eslam.amin.610@gmail.com>
2026-05-26 01:54:11 +0300

    feat: install, and import helmet to the project

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import seatRoutes from "./routes/seat.routes";
 import { globalErrorHandler } from "./middlewares/error.middleware";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "../swagger-docs.json"; // Ensure resolveJsonModule is true in tsconfig
+import helmet from "helmet";
 
 const app: Application = express();
 
+app.use(helmet());
 app.use(express.json());
 const apiV1 = "/api/v1";
 // Mount Interactive Explorer UI endpoint
